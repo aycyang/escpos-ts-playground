7a9fb9f65cb572f2b3966117833c1fdb990e261f
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-11-16 18:24:09 +0800

    Parse as you type

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,49 @@
+import { Buffer } from 'buffer'
 import * as escpos from 'escpos-ts'
 
 window.onload = () => {
-  const inputTextarea = document.getElementById('input')
+  const inputTextarea: HTMLTextAreaElement = document.getElementById('input') as HTMLTextAreaElement
+  inputTextarea.addEventListener('input', event => {
+    const buf = parseHex(inputTextarea.value)
+    let cmds = []
+    try {
+      cmds = escpos.parse(buf)
+      displayCmds(cmds)
+      displayError('')
+    } catch (e) {
+      displayCmds([])
+      displayError(e)
+    }
+  })
+}
+
+function displayError(error) {
+  const errorDiv = document.getElementById('error')
+  clear(errorDiv)
+  errorDiv.innerHTML = error
+}
+
+function displayCmds(cmds) {
   const outputDiv = document.getElementById('output')
-  const cmds = [
-    new escpos.InitializePrinter(),
-    new escpos.SelectJustification(escpos.Justification.Center),
-  ]
+  clear(outputDiv)
   const rows = cmds.map(cmd => tr(td(formatHex(cmd.serialize())), td(cmd.toString())))
-  const table = document.createElement('table')
+  let table = document.createElement('table')
   for (const row of rows) {
     table.appendChild(row)
   }
   outputDiv.appendChild(table)
+}
+
+function parseHex(hex: string): Buffer {
+  hex = hex.replace(/[ \t\r\n]/g, '')
+  const buf = Buffer.from(hex, 'hex')
+  return buf
+}
+
+function clear(element: HTMLElement) {
+  while (element.hasChildNodes()) {
+    element.removeChild(element.lastChild)
+  }
 }
 
 function tr(...args) {
@@ -33,7 +64,7 @@ function formatHex(buf: any): string {
   const chunks: string[] = []
   for (let i = 0; i < buf.length; i++) {
     chunks.push(buf.subarray(i, i+1).toString('hex'))
-    if (chunks.length > 16) {
+    if (chunks.length > 5) {
       chunks.pop()
       chunks.push('...')
       break
